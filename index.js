52a918d05721da4b645cdf7b02f56e342b672ab0
York <helloyork@icloud.com>
2024-03-20 07:56:18 -0700

    AIAPA v0.1.10

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ export const Commands = app.App.Commands;
 app.registerProgram({
     name: "AIAPA",
     description: "AI Analyzes Products on Amazon",
-    version: "0.1.9",
+    version: "0.1.10",
 })
     .registerCommands(Commands)
     .startIf(isImported(import.meta.url));
